ada8a7def4aaccc9090dda83e66eca80b1515018
MOBUMIN <kiju23@naver.com>
2021-01-08 00:58:19 +0900

    add login
    bycript, cookie-parser, jwt를 이용하여 로그인 기능 구현

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
+const cookieParser = require('cookie-parser');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require('./config/key');
 const mongoose = require('mongoose');
@@ -23,6 +26,34 @@ app.post('/register',(req,res)=>{
     user.save((err,userInfo)=>{
         if(err) return res.json({success:false,err})
         return res.status(200).json({success:true})
+    })
+})
+
+//login 기능
+app.post('/login', (req,res)=>{
+    //요청된 이메일을 데이터베이스에서 찾기
+    User.findOne({ email:req.body.email }, (err, user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess:false,
+                message: "유저가 없습니다."
+            })
+        }
+        //이메일이 있다면 비밀번호가 같은지 확인
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch)
+            return res.json({
+                loginSuccess: false,
+                message: " 비밀번호가 틀림 "
+            })
+            //비밀번호가 맞다면 Token 생성
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+
+                //토큰을 저장한다. => 쿠키, 로컬스토리지 ....
+                res.cookie("x_auth", user.token).status(200).json({loginSuccess:true, userId: user._id})
+            })
+        })
     })
 })
 
